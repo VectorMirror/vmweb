23aeb5101511a07a7e424bd5eb3962739261396c
MiguelPenha <miguel.pena@graphicsoft.com>
2021-04-02 16:32:08 -0600

    Version Primigenia

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>.:Vector~Mirror:.</title>
+</head>
+<body>
+    <?php
+        require_once ('header.html');
+    ?>
+
+    <?php
+        require_once ('main.php');
+    ?>
+
+    <?php
+        require_once ('aside.php');
+    ?>
+
+    <?php
+        require_once ('footer.php');
+    ?>
+</body>
+</html>
